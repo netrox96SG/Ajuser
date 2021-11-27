9f2ac9965ff6b3ff80d0adf9f5a94098da802f4e
Mr soumya <94045673+netrox96SG@users.noreply.github.com>
2021-11-27 09:58:44 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -6,16 +6,16 @@ if (Config.WORKTYPE == 'private') {
 
     Julie.addCommand({pattern: 'owner', fromMe: true, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '917025868709-1634557305@g.us') {
+        if (message.jid === '919776517392-1634557305@g.us') {
 
             return;
         }
 
-        if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*AJUSER created by aju*' , MessageType.text);
+        if (Config.OWNER == 'Mr soumya') {
+            await message.client.sendMessage(message.jid,'*MR SOUMYA created by BLACK DRAGON*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n AJUSER created by aju', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER +919776517392 '\n Mr soumya created by BLACK DRAGON', MessageType.text);
         }
     }));
 }
@@ -24,16 +24,16 @@ else if (Config.WORKTYPE == 'public') {
 
     Julie.addCommand({pattern: 'owner', fromMe: false, desc: 'shows the detail of bot owner'}, (async (message, match) => {
 
-        if (message.jid === '917025868709-1634557305@g.us') {
+        if (message.jid === '919776517392-1634557305@g.us') {
 
             return;
         }
 
-        if (Config.OWNER == 'default') {
-            await message.client.sendMessage(message.jid,'*AJUSER created by aju*' , MessageType.text);
+        if (Config.OWNER == 'Mr soumya') {
+            await message.client.sendMessage(message.jid,'*MR SOUMYA created by BLACK DRAGON*' , MessageType.text);
         }
         else {
-            await message.client.sendMessage(message.jid,Config.OWNER + '\n AJUSER created by aju', MessageType.text);
+            await message.client.sendMessage(message.jid,Config.OWNER +919776517392'\n MR SOUMYA created by Black dragon', MessageType.text);
         }
     }));
 }
